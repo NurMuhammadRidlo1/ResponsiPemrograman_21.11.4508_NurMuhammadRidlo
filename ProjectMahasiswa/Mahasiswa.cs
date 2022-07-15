2cce7c7b12b336ac9eb1ea38891def20097eb458
Nur Muhammad Ridlo <nurmuhammadridlo@gmail.com>
2022-07-15 10:16:38 +0700

    First Commit

---
+++ ProjectMahasiswa/Mahasiswa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectMahasiswa
+{
+    public class Mahasiswa
+    {
+        // PERINTAH: lengkapi property class mahasiswa, sesuai petunjuk soal
+        public int NIM { get; set; }
+        public string Nama { get; set; }
+        public string JenisKelamin { get; set; }
+        public double Ipk { get; set; }
+
+    }
+}
